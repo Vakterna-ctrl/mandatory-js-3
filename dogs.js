045b574a7968636f43dc0a2e5def9f9efc545c5b
Chau Kha Doan <kha.doanchau@gmail.com>
2019-12-13 05:03:05 +0100

    added sub breed

--- dogs.js
+++ dogs.js
@@ -1,15 +1,24 @@
 let header = document.querySelector('header')
 let article = document.querySelector('article');
 let dogsMessage;
+let breedNameSaver;
 header.addEventListener('click', function(e){
   if(e.target.textContent === "Go Back"){
-    window.location = "";
+    header.innerHTML = "";
     getDogsNames();
     getDogPics('https://dog.ceo/api/breeds/image/random/3');
-  }else{
+  }
+  else if (e.target.className === 'subBreed') {
+    header.innerHTML = "";
+    window.location.hash = e.target.textContent;
+    renderMenu("Go Back")
+    getDogPics('https://dog.ceo/api/breed/' + breedNameSaver + '/' + e.target.textContent + '/images/random/3');
+  }
+
+  else if(e.target.className === ""){
+  header.innerHTML = "";
+  breedNameSaver = e.target.textContent;
   window.location.hash = e.target.textContent;
-  console.log(e.target.textContent)
-  console.log('https://dog.ceo/api/breed/' + e.target.textContent + '/images/random/3')
   getDogBreed(e.target.textContent)
   getDogPics('https://dog.ceo/api/breed/' + e.target.textContent + '/images/random/3');
 }
@@ -17,18 +26,22 @@ header.addEventListener('click', function(e){
 window.addEventListener('load', function(e){
   if(window.location.hash !== ''){
     let removeHash = window.location.hash.split('#')[1]
-    getDogBreed(removeHash)
-    getDogPics('https://dog.ceo/api/breed/' + removeHash + '/images/random/3');
-  }
+       getDogPics('https://dog.ceo/api/breed/' + removeHash + '/images/random/3');
+       getDogBreed(removeHash)
 
+  }else{
+    getDogsNames();
+    getDogPics('https://dog.ceo/api/breeds/image/random/3');
+  }
 })
 
-function renderMenu(name){
+function renderMenu(name,breed){
   let newTextElement = document.createElement('div');
   newTextElement.style.width = "110px";
   newTextElement.style.height = "40px";
   newTextElement.style.color = "white";
   newTextElement.style.cursor = "pointer";
+  newTextElement.className = breed;
   newTextElement.textContent = name;
   header.appendChild(newTextElement);
 }
@@ -42,7 +55,7 @@ xhr.addEventListener('load', function(e){
   dogsMessage = dogs.message;
   let dogNames = Object.keys(dogsMessage)
   for(dog of dogNames){
-    renderMenu(dog);
+    renderMenu(dog,"");
   }
 }else{
   alert('oops')
@@ -51,7 +64,6 @@ xhr.addEventListener('load', function(e){
 xhr.open('GET', 'https://dog.ceo/api/breeds/list/all');
 xhr.send();
 }
- getDogsNames();
 
  function getDogPics(breedName){
    article.innerHTML = "";
@@ -88,16 +100,15 @@ xhr.send();
    xhr.open('GET', breedName);
    xhr.send();
  }
-getDogPics('https://dog.ceo/api/breeds/image/random/3');
 
 function getDogBreed(nameOfDog){
   header.innerHTML ="";
-  if(dogsMessage[nameOfDog].length != 0){
+  if(dogsMessage[nameOfDog].length !== 0){
     renderMenu("Go Back");
     for (dog of dogsMessage[nameOfDog]) {
-      renderMenu(dog);
+      renderMenu(dog,'subBreed');
     }
   }else{
-    renderMenu("Go Back");
+    renderMenu("Go Back", "");
   }
 }
